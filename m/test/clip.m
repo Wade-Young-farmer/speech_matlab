e04461effc89f20852182341cab8a27b6a022404
daiyi02 <daiyi02@baidu.com>
2019-12-25 11:07:53 +0800

    opt clip to take wav file.

--- m/test/clip.m
+++ m/test/clip.m
@@ -1,16 +1,22 @@
-function clip(pcm_name)
-Srate=16000;
-t= 25;
-
-file_id=fopen(pcm_name, 'r');
-x=fread(file_id, inf, 'int16');
-fclose(file_id);
-
-s=regexp(pcm_name, '\.', 'split');
+function clip(file_name)
+s=regexp(file_name, '\.', 'split');
 outfile=strcat(s(1),'_short.',s(2));
 outfile=char(outfile);
-xfinal=x(1:t*Srate);
-file_id=fopen(outfile,'wb');
-fwrite(file_id,xfinal,'int16');
-fclose(file_id);
+if strcmp(s(2),'pcm')
+    Srate=16000;
+    t= 25;
+
+    file_id=fopen(file_name, 'r');
+    x=fread(file_id, inf, 'int16');
+    fclose(file_id);
+    
+    xfinal=x(1:t*Srate);
+    file_id=fopen(outfile,'wb');
+    fwrite(file_id,xfinal,'int16');
+    fclose(file_id);
+elseif strcmp(s(2), 'wav')
+    [x, fs] = audioread(file_name);
+    x=x(fs*9 + 1:fs*14,:);
+    audiowrite(outfile,x,fs);
+end
 end
--- m/test/run_clip.m
+++ m/test/run_clip.m
@@ -1,5 +1,5 @@
 clear all;
 close all;
-clip('/Users/daiyi02/baidu/audio-alg/audio-process-data/sample/xiaoyu_3_1/nz75db_3m_d3/rec_mic_0_0.pcm');
+clip('/Users/daiyi02/Downloads/farend_nearend_dt_st_16bit/ref_dsp_dump_stereo.wav');
 % clip('rec_mic_1_0.pcm');
 % clip('rec_spk_l_0.pcm');
